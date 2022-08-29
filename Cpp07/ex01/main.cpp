886d00ec13c8a3607fdd54967ed3da69d422f910
Issam Jmari <issamjmari098@gmail.com>
2022-08-29 17:14:14 +0100

    OLK

--- Cpp07/ex01/main.cpp
+++ Cpp07/ex01/main.cpp
@@ -3,9 +3,13 @@
 
 int main()
 {
-	TestClass a(5);
-	TestClass b(6);
-	TestClass c(7);
-	TestClass arr[3] = {a, b, c};
-	iter(arr, 3, f<TestClass>);
+	std::cout << "char array : \n";
+	char char_arr[] = {'a', 'v', 'z', 't', 'f'};
+	iter(char_arr, 5, f<char>);
+	std::cout << "int array : \n";
+	int int_arr[] = {25400, 4, 1, 190};
+	iter(int_arr, 4, f<int>);
+	std::cout << "TestClass array : \n";
+	TestClass test_arr[3] = {TestClass(99), TestClass(80), TestClass(30)};
+	iter(test_arr, 3, f<TestClass>);
 }
---
+++ Cpp07/ex02/Array.hpp
@@ -0,0 +1,48 @@
+#ifndef ARRAY_HPP
+#define ARRAY_HPP
+	#include <iostream>
+	#include <exception>
+	template <typename T> 
+	class Array
+	{
+		private:
+			T *arr;
+			unsigned int s;
+		public:
+			Array(void)
+			{
+				arr = new T();
+			}
+			Array(unsigned int n) : s(n)
+			{
+				arr = new T[n]();
+			}
+			Array(const Array &ref)
+			{
+				this->s = ref.s;
+				arr = new T[s];
+				for(unsigned int i = 0; i < s; i++)
+					this->arr[i] = ref.arr[i];
+			}
+			Array &operator=(const Array &ref)
+			{
+				this->s = ref.s;
+				if(arr != NULL)
+					delete [] arr;
+				arr = new T[s];
+				for(unsigned int i = 0; i < s; i++)
+					this->arr[i] = ref.arr[i];
+				return (*this);
+			}
+			T &operator[](unsigned int i) 
+			{
+				if(i > s)
+					throw std::out_of_range("Out of range");
+				return arr[i];
+        	}
+			unsigned int size(void) const
+			{
+				return (s);
+			}
+	} ;
+#endif
